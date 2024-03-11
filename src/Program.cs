a11317ec6e9b844c45f287c5da020bbae7358773
Callum Jules White <62078259+juleswhi@users.noreply.github.com>
2024-03-11 15:03:36 +0000

    Add Puzzles file to SC

--- src/Program.cs
+++ src/Program.cs
@@ -1,26 +1,18 @@
-using System.Data.Common;
-using System.Diagnostics;
-using System.Text.Json;
-using Chess;
-using ChessMasterQuiz.Forms;
-using ChessMasterQuiz.Chess;
+namespace ChessMasterQuiz;
 
-namespace ChessMasterQuiz
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    public static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        public static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
 
-            Application.Run(new formMain());
-                
-        }
+        Application.Run(new formMain());
+            
     }
 }
